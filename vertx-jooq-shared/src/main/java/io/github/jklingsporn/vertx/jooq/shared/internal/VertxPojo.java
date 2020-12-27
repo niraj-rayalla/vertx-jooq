e454af2a817e795f831fe3143c313a9d87050d5d
niraj-rayalla <niraj.rayalla@gmail.com>
2020-12-26 23:46:22 -0600

    Added setOrThrow() in VertxPojo that can take a Consumer<T> as the pojoSetter.

--- vertx-jooq-shared/src/main/java/io/github/jklingsporn/vertx/jooq/shared/internal/VertxPojo.java
+++ vertx-jooq-shared/src/main/java/io/github/jklingsporn/vertx/jooq/shared/internal/VertxPojo.java
@@ -2,6 +2,7 @@ package io.github.jklingsporn.vertx.jooq.shared.internal;
 
 import io.github.jklingsporn.vertx.jooq.shared.UnexpectedJsonValueTypeException;
 
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 /**
@@ -37,6 +38,26 @@ public interface VertxPojo {
     public static <T> void setOrThrow(Function<T,?> pojoSetter, Function<String,T> jsonGetter, String fieldName, String expectedFieldType){
         try {
             pojoSetter.apply(jsonGetter.apply(fieldName));
+        } catch (java.lang.ClassCastException e) {
+            throw new UnexpectedJsonValueTypeException(fieldName,expectedFieldType,e);
+        }
+    }
+
+    /**
+     * For internal purposes only.<br>
+     * Function to safely set a value inside a POJO. Helps users to find bugs if they accidentally put a value into
+     * a {@code JsonObject} with a wrong type.
+     * @param pojoSetter the setter of this POJO's property
+     * @param jsonGetter the function to obtain the value from the {@code JsonObject}
+     * @param fieldName the name of the POJO / JSON-property
+     * @param expectedFieldType the type of the property/field
+     * @param <T> the value type
+     * @see <a href="https://github.com/jklingsporn/vertx-jooq/pull/110">Related PR</a>
+     * @see <a href="https://github.com/jklingsporn/vertx-jooq/issues/170">Related Issue</a>
+     */
+    public static <T> void setOrThrow(Consumer<T> pojoSetter, Function<String,T> jsonGetter, String fieldName, String expectedFieldType){
+        try {
+            pojoSetter.accept(jsonGetter.apply(fieldName));
         } catch (java.lang.ClassCastException e) {
             throw new UnexpectedJsonValueTypeException(fieldName,expectedFieldType,e);
         }
